39743a2f26348e89b2d83f520e5a44d8dc28a195
Artyom Zhirov <azhirov@tradingview.com>
2024-11-20 20:44:50 +0300

    prepare client's code without result output

--- client.go
+++ client.go
@@ -10,8 +10,10 @@ import (
 
 func main() {
 
+	// Куда будем отправлять запрос
 	url := "http://localhost:8081/watermark"
 
+	// Открываем картинку, которую будем отправлять (она вообще есть?)
 	file, err := os.Open("perf1.png")
 	if err != nil {
 		fmt.Println("Error opening perf1.png file", err)
@@ -19,33 +21,36 @@ func main() {
 	}
 	defer file.Close()
 
+	// Читаем картинку
 	fileBytes, err := ioutil.ReadAll(file)
 	// image, _, err := r.FormFile("image") Это получает картинку из формы. А Выше из файла
 	if err != nil {
-		fmt.Println(er)
+		fmt.Println(err)
 		return
 	}
-
 	payload := bytes.NewReader(fileBytes)
 
+	// Готовим POST запрос
 	req, err := http.NewRequest("POST", url, payload)
 	if err != nil {
 		fmt.Println(err)
 	}
+	req.Header.Add("Content-Type", "image/png")
 
-	req.Header.Add("Content-Type", "text/csv")
-
+	// Отправляем подготовленный запрос
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	defer res.Body.Close()
+
+	// Читаем тело ответа
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Дополнительно выводим ответ и тело ответа (в формате строки)
 	fmt.Println(res)
 	fmt.Println(string(body))
 
